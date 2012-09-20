588ff83c39728bd9db492607582853ee0c7b7d14
goran-despotoski <goran2005@yahoo.com>
2012-09-20 18:36:58 +0200

    Commit after copy pasting and overwriting with proper file

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
 <?php
-echo "aaaaaaaaaaaaaaaaarrghhhh, GIT please wooooooooorkkkkkkkkk!!!!!!";
+session_start();
+date_default_timezone_set('Europe/Skopje');
+error_reporting(E_ALL);
+
+require_once 'system/base/error_handler.php';
+//configuration
+require_once 'system/base/GlobalRegistry.php';
+require_once 'application/config/config.php'; //config uses GlobalRegistry so this should always be below includin GlobalRegistry.php
+//basic functionality
+require_once 'system/base/DataAccess.php';
+require_once 'system/base/controller.php';
+require_once 'system/base/model.php';
+require_once 'system/base/dispatcher.php';
+
+$dispatcher_instance = new Dispatcher(); 
 ?>
