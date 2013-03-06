896e5dd97e5f31b18fd549d04e8beff035458eb2
goran-despotoski <goran2005@yahoo.com>
2013-03-06 11:33:39 +0100

    Core env files are now loaded first, so that settings in them can be overriden by separate application env files

--- application/index.php
+++ application/index.php
@@ -10,11 +10,11 @@ require_once $corePath . 'base/GlobalRegistry.php';
 
 $global = GlobalRegistry::getInstance();
 
-require_once $appPath . "env/env.inc.php";
-require_once $appPath . "env/db.inc.php";
-
 require_once $corePath . "env/env.inc.php";
 require_once $corePath . "env/db.inc.php";
+
+require_once $appPath . "env/env.inc.php";
+require_once $appPath . "env/db.inc.php";
 
 require_once $corePath . 'bootstrap.php';
 ?>
